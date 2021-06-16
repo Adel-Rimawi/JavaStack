b1e21bc87a948c3097a2254eae7046f42f5de28d
Adel-Rimawi <a.s.riimawi@gmail.com>
2021-06-16 20:06:25 +0300

    adding Strings Assignment

---
+++ Strings/src/test/java/com/Axsos/Java/StringsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Axsos.Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
